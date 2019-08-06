10ba352d1c7da9d9228902a3fa15836a36c1d3aa
Kuramshin Rustam <rkuramshin@tradingview.com>
2019-08-06 16:27:53 +0300

    add two test pages

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"bytes"
+	"fmt"
+	"github.com/PuerkitoBio/goquery"
 	"github.com/gorilla/mux"
 	"io/ioutil"
 	"log"
@@ -11,8 +13,6 @@ import (
 	"net/url"
 	"strconv"
 	"time"
-
-	"github.com/PuerkitoBio/goquery"
 )
 
 var targetUrl string
@@ -21,20 +21,21 @@ var proxySrcUrl string
 func main() {
 
 	router := mux.NewRouter()
+	router.HandleFunc("/pageproxy", pageProxyHandle).Methods("GET")
+	router.HandleFunc("/resourceproxy", resourceProxyHandle).Methods("GET")
 	router.HandleFunc("/proxy", proxyHandle).Methods("GET")
-	router.HandleFunc("/srcproxy", srcproxyHandle).Methods("GET")
-	router.PathPrefix("/").Handler(http.FileServer(http.Dir("./web/")))
+	router.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
 
 	log.Println("Server starting")
 	// log.Fatal(http.ListenAndServeTLS(":9090", "cert.pem", "key.pem", router))
 	log.Fatal(http.ListenAndServe(":9090", router))
 }
 
-func proxyHandle(res http.ResponseWriter, req *http.Request) {
+func pageProxyHandle(res http.ResponseWriter, req *http.Request) {
 
 	queryParams := req.URL.Query()
-	proxyUrl, _ := url.Parse(queryParams["url"][0])
 	targetUrl := queryParams["url"][0]
+	proxyUrl, _ := url.Parse(queryParams["url"][0])
 
 	director := func(req *http.Request) {
 		req.URL.Scheme = proxyUrl.Scheme
@@ -73,12 +74,12 @@ func proxyHandle(res http.ResponseWriter, req *http.Request) {
 		if err == nil {
 			doc.Find("link").Each(func(i int, s *goquery.Selection) {
 				if valHref, exst := s.Attr("href"); exst {
-					s.SetAttr("href", "http://localhost:9090/srcproxy?url="+targetUrl+valHref)
+					s.SetAttr("href", "/resourceproxy?url="+targetUrl+valHref)
 				}
 			})
 			doc.Find("script").Each(func(i int, s *goquery.Selection) {
 				if valHref, exst := s.Attr("src"); exst {
-					s.SetAttr("src", "http://localhost:9090/srcproxy?url="+targetUrl+valHref)
+					s.SetAttr("src", "/resourceproxy?url="+targetUrl+valHref)
 				}
 			})
 		}
@@ -86,21 +87,42 @@ func proxyHandle(res http.ResponseWriter, req *http.Request) {
 		html, _ := doc.Html()
 		bodyByte := []byte(html)
 		proxyRes.Body = ioutil.NopCloser(bytes.NewReader(bodyByte))
-
 		proxyRes.ContentLength = int64(len(bodyByte))
 		proxyRes.Header.Set("Content-Length", strconv.Itoa(len(bodyByte)))
 
-		log.Println("Server response received")
-		//if body, err := httputil.DumpResponse(proxyRes, true); err == nil {
-		//	//fmt.Println(string(body))
-		//}
+		log.Println("Return to client")
+		if body, err := httputil.DumpResponse(proxyRes, true); err == nil {
+			fmt.Println(string(body))
+		}
+
 		return nil
 	}
 
 	proxy.ServeHTTP(res, req)
 }
 
-func srcproxyHandle(res http.ResponseWriter, req *http.Request) {
+func resourceProxyHandle(res http.ResponseWriter, req *http.Request) {
+
+	// TODO: js modifyzer
+
+	queryParams := req.URL.Query()
+	proxyUrl, _ := url.Parse(queryParams["url"][0])
+
+	proxy := httputil.NewSingleHostReverseProxy(&url.URL{
+		Scheme: proxyUrl.Scheme,
+		Host:   proxyUrl.Host,
+	})
+
+	req.Host = proxyUrl.Host
+	req.URL.Host = proxyUrl.Host
+	req.URL.Path = proxyUrl.Path
+	req.URL.Scheme = proxyUrl.Scheme
+	req.RequestURI = proxyUrl.Path
+
+	proxy.ServeHTTP(res, req)
+}
+
+func proxyHandle(res http.ResponseWriter, req *http.Request) {
 
 	queryParams := req.URL.Query()
 	proxyUrl, _ := url.Parse(queryParams["url"][0])
@@ -117,6 +139,11 @@ func srcproxyHandle(res http.ResponseWriter, req *http.Request) {
 	req.RequestURI = proxyUrl.Path
 
 	proxy.ModifyResponse = func(proxyRes *http.Response) error {
+		proxyRes.Header.Del("X-Frame-Options")
+		log.Println("Return to client")
+		if body, err := httputil.DumpResponse(proxyRes, true); err == nil {
+			fmt.Println(string(body))
+		}
 		return nil
 	}
 
